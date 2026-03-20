2c040f4bd2f8699efc3a29cbebadd7181b468d05
noeltz <161815407+noeltz@users.noreply.github.com>
2026-03-20 11:10:53 +0100

    Update build.py

--- build.py
+++ build.py
@@ -1184,10 +1184,6 @@ def build_nf_by_prebuild_nerd_font(
     suffix = font_config.get_nf_suffix()
     if suffix:
         suffix = "-" + suffix
-
-    nf_type = font_config.get_nf_suffix() # Returns "Mono" or "Propo"
-    if nf_type:
-        suffix += f"-{nf_type}"
     
     nf_base_font_path = f"{build_option.src_dir}/MapleMono-NF-Base{suffix}.ttf"
     tmp_target_path = None
